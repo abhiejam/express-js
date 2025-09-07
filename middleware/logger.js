454f88011266b5fb3cc38c677b6278d63829625d
abhiejam <ejam.abhishek@gmail.com>
2025-09-07 16:02:26 +1000

    fix: add PUT and DELETE methods in logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,8 +4,10 @@ const logger = (req, res, next) => {
     const methodColors = {
         POST: 'blue',
         GET: 'green',
+        PUT: 'black',
+        DELETE: 'red'
     };
-    const color = methodColors[req.method] || white;
+    const color = methodColors[req.method] || methodColors['white'];
     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[color]);
     next();
 };
